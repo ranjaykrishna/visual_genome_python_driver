cabea4c73c3a24394f1a801aa4a5590273fde0b7
Eric B <ebigelow@u.rochester.edu>
2016-06-13 10:28:59 -0400

    Added methods to load locally saved scene graph data.

--- src/local.py
+++ src/local.py
@@ -50,3 +50,87 @@ def GetAllQAs(dataDir=None):
     output.append(utils.ParseQA(image['qas'], imageMap))
   return output
 
+
+"""
+Convert list of objects with `id` attribute to dictionary indexing objects by id.
+
+"""
+def ListToDict(ls):
+  return {obj.id:obj for obj in ls}
+
+
+"""
+Get all scene graphs.
+
+"""
+def GetAllSceneGraphs(dataDir='data/', imageDataDir='data/by-id/'):
+  images = ListToDict(GetAllImageData(dataDir))
+  scene_graphs = []
+
+  for fname in os.listdir(imageDataDir):
+    image_id = fname.split('.')[0]
+    image = images[image_id]
+    data = json.load(open(imageDataDir + fname, 'r'))
+
+    scene_graph = utils.ParseGraph(data, image)
+    scene_graphs.append(scene_graph)
+
+  return scene_graphs
+
+
+def GetSceneGraphs(image_id, images='data/', imageDataDir='data/by-id/'):
+  if type(images) is str:
+    images = ListToDict(GetAllImageData(images))
+
+  fname = str(image_id) + '.json'
+  image = images[str(image_id)]
+  data = json.load(open(imageDataDir + fname, 'r'))
+
+  scene_graph = utils.ParseGraph(data, image)
+  return scene_graph
+
+
+
+
+"""
+Save a unique json file for each image id; required for GetSceneGraphs.
+
+Each json has the following info:
+  - 'id'
+  - 'attributes'
+  - 'objects'
+  - 'relationships'
+
+"""
+def SaveById(dataDir='data/', imageDataDir='data/by-id/'):
+  import gc
+  if not os.path.exists(imageDataDir): os.mkdir(imageDataDir)
+
+  fnames = ['attributes','objects','relationships']
+  for fname in fnames:
+
+    with open(dataDir + fname + '.json', 'r') as f:
+      a = json.load(f)
+
+    for item in a:
+      iid = item['id']
+      ifname = imageDataDir + str(iid) + '.json'
+
+      if os.path.exists(ifname):
+        fi = open(ifname, 'a')
+        data = json.load(fi)
+      else:
+        fi = open(ifname, 'w')
+        data = {'id' : iid}
+
+      data[fname] = item[fname]
+      json.dump(data, fi)
+      fi.close()
+
+    del a
+    gc.collect()
+
+
+
+
+
